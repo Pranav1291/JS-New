2788c42f9d41a35040e26ce3549e6ebb71f32520
Pranav Srivastava <109536484+Pranav1291@users.noreply.github.com>
2023-09-14 04:19:38 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,23 @@
+const name = "Pranav"
+const repoCount = 10
+
+//console.log(name + repoCount + "YOOO")
+
+console.log(`Hello my name is ${name} and I have ${repoCount} repositories`);
+
+let gameName = new String('Battleground India')
+
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('g'));
+
+console.log(gameName.substring(0, 6))
+console.log(gameName.includes('bound'));
+console.log(gameName.includes('nd'));
+console.log(gameName.split(' '));
+console.log(gameName.__proto__);
+console.log(gameName.slice(0, 6));
+console.log(gameName.trim(0, 6));
+console.log(gameName.concat('Pranav')); //appends
+
+
+
