c81bc8df44f344d1f9fe50859730c62a5d46a4be
Pranav Srivastava <109536484+Pranav1291@users.noreply.github.com>
2023-09-15 11:46:19 +0000

    mathandnums

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,34 @@
+const score = 400
+//console.log(score)
+
+const balance = new Number(100)
+//console.log(balance)
+
+//console.log(balance.toString());
+//console.log(balance.toFixed(2));
+
+const newNumber = 123.8977;
+
+//console.log(newNumber.toPrecision(4));
+
+const hundreds = 1000000;
+//console.log(hundreds.toLocaleString('en-US'));
+
+
+//############################## Maths ##############################
+
+// console.log(Math);
+// console.log(Math.abs(4)); //absolute value
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.0));
+// console.log(Math.floor(4.1)); 
+// console.log(Math.min()); //for minimum value
+
+console.log(Math.random()); //random value aati hai 0 aur 1 ke beech
+console.log((Math.random()*10) + 1); //1 se upr chhie uske liye ye formula
+
+const min = 10 // minimum itna chhie 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); // min. 10 chhie hi iske lie ye formula
+
